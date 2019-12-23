656782ab17f6094b788a1ae8fa76f2ae7da91036
Dale Wang <dalinwanglhtz@gmail.com>
2019-12-23 23:49:30 +1100

    final working cut

--- src/com/codewardev/Kata.java
+++ src/com/codewardev/Kata.java
@@ -1,20 +1,13 @@
 package com.codewardev;
 
+import java.util.Arrays;
+
 public class Kata {
 
 	public static String createPhoneNumber(int[] numbers) {
-		String result = "(";
-		for(int i=0; i<numbers.length; i++) {
-			if(i==3) {
-				result += ") "+numbers[i];
-			} else
-			if(i==6) {
-				result += "-"+numbers[i];
-			} else {
-				result += numbers[i];
-			}
-		}
-		return result;
+		String result = Arrays.toString(numbers).replaceAll(", ", "").replaceAll("(\\d{3})(\\d{3})(\\d{4})", "($1) $2-$3");
+		
+		return result.substring(1, result.length()-1);
 	}
 
 }
